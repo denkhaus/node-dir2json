ae270820df3f25a7c97163e18a9f24e9d0a53ac9
denkhaus <peristaltic@gmx.net>
2014-04-18 21:09:04 +0200

    modified processing

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 
 var fs = require('fs'),
+    _ = require("underscore"),
     path = require('path');
 
-module.exports = function(filename) {
+module.exports = function dirTree(filename, includeDotFiles) {
     var stats = fs.lstatSync(filename),
+        bName = path.basename(filename),
         info = {
             path: filename,
-            label: path.basename(filename)
+            label: bName
         };
 
+    if (!includeDotFiles &&
+        bName.substring(0, 1) == "."){
+        return null;
+    }
+
     if (stats.isDirectory()) {
-        info.type = "dir";
-        info.children = fs.readdirSync(filename).map(function(child) {
-            return dirTree(filename + '/' + child);
+        var childs = fs.readdirSync(filename).map(function(child){
+           return dirTree(filename + '/' + child, includeDotFiles);
         });
+
+        childs = _.reject(childs, function(elm){ return elm === null; });
+        info.type = "dir";
+
+        if(childs.length > 0){
+            info.children = childs;
+        }
+
     } else if( stats.isFile()){
         info.type = "file";
     }
